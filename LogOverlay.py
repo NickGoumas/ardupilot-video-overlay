97842dcd356684647e0c09927e26cc6413483cfa
NickGoumas <nicholasmarkgoumas@gmail.com>
2016-12-23 00:29:47 -0500

    Most recent local version.

--- LogOverlay.py
+++ LogOverlay.py
@@ -11,8 +11,11 @@ parser.add_argument('-s', '--start', help='time in sec at arming')
 parser.add_argument('-e', '--end', help='time in sec at video end')
 args = parser.parse_args()
 
+timeStart = int(args.start)
+timeEnd = int(args.end)
+clipDur = timeEnd - timeStart
+clipList = []
 flightModes = {0:'Stabilize', 1:'Acro', 2:'AltHold', 3:'Auto', 4:'Guided', 5:'Loiter', 6:'RTL', 7:'Circle', 8:'Position', 9:'Land', 10:'OF_Loiter', 11:'Drift', 13:'Sport', 14:'Flip', 15:'AutoTune', 16:'PosHold', 17:'Brake' }
-
 
 log_df = pd.read_csv(args.log_file)
 
@@ -23,28 +26,30 @@ while True:
     else:
         start_time = log_df['GPS_TimeUS'].loc[FirstGoodRow]
         break
+
+
 for i in range(FirstGoodRow, len(log_df['GPS_TimeUS'])):
-    val = int(log_df['GPS_TimeUS'].iloc[i] - start_time) / 1000000
+    val = int((log_df['GPS_TimeUS'].iloc[i] - start_time) / 1000000)
     log_df.set_value(i, 'GPS_TimeUS', val)
+
+print log_df['GPS_TimeUS'].tolist()
 
 
 def lookup(_second, col):
     for i in range(0, len(log_df)):
         if log_df['GPS_TimeUS'].iloc[i] >= _second:
             if col == 'MODE_ModeNum':
-                #return int(log_df[col].iloc[i])
                 return flightModes.get(int(log_df[col].iloc[i]))
             break
     return log_df[col].iloc[i]
 
 
-timeStart = int(args.start)
-timeEnd = int(args.end)
-clipDur = timeEnd - timeStart
 
-clipList = []
+
+
 
 #txt_title = ( TextClip('Testing', fontsize=80, font='FreeMono', color='white').set_position('center').set_duration(1).set_start(1))
+
 txt_result = ( TextClip('                                                            ', fontsize=32, font='FreeMono', color='black').set_duration(1).set_start(1) )
 
 for i in range(2, clipDur):
